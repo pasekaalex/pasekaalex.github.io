094a1ee121c2459094876eb298c1af9519845819
Alex Paseka <pasekaalex@gmail.com>
2019-04-05 20:54:59 -0400

    CISC3130 Homework#3

--- hw3/App.java
+++ hw3/App.java
@@ -8,6 +8,7 @@ import com.kennycason.kumo.bg.PixelBoundryBackground;
 import com.kennycason.kumo.CollisionMode;
 import com.kennycason.kumo.WordCloud;
 import com.kennycason.kumo.WordFrequency;
+import com.kennycason.kumo.font.KumoFont;
 import com.kennycason.kumo.font.scale.LinearFontScalar;
 import java.awt.*;
 import java.io.*;
@@ -19,16 +20,21 @@ public class App {
         frequencyAnalyzer.setMinWordLength(2);
 
 
-        final java.util.List<WordFrequency> wordFrequencies = frequencyAnalyzer.load("WordFrequencyDataResults.txt");
-
+        final java.util.List<WordFrequency> wordFrequencies = frequencyAnalyzer.load("Lyrics.txt");
+        
+        final Font[] FONTS = new Font[] { 
+                new Font("Calibri", Font.PLAIN, 10)
+        };
+        
         final Dimension dimension = new Dimension(600, 498);
         final WordCloud wordCloud = new WordCloud(dimension, CollisionMode.PIXEL_PERFECT);
         wordCloud.setPadding(2);
         wordCloud.setBackground(new PixelBoundryBackground("stairs.png"));
-        wordCloud.setFontScalar(new LinearFontScalar(4, 10));
-        wordCloud.setBackgroundColor(new Color(0xFFFFFF));
-        wordCloud.setColorPalette(new ColorPalette(new Color(0xAA0000), new Color(0xFFFFFF)));
-
+        wordCloud.setKumoFont(new KumoFont(FONTS[0]));
+        wordCloud.setFontScalar(new LinearFontScalar(10, 40));
+        wordCloud.setBackgroundColor(null);
+        wordCloud.setColorPalette(new ColorPalette(new Color(0x664389),
+        		new Color(0x552299), new Color(0x331166)));
 
         wordCloud.build(wordFrequencies);
         wordCloud.writeToFile("Stairway.png");
