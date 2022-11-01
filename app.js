a5bbb70709647a3676da5b67e4acc91fb4526ae1
pasekaalex <pasekaalex@gmail.com>
2022-11-01 18:50:43 -0400

    im not wrong

--- app.js
+++ app.js
@@ -55,17 +55,19 @@ const createTiles = quantity => {
 
 const createGrid = () => {
     wrapper.innerHTML = "";
+    
     const size = document.body.clientWidth > 800 ? 100 : 50;
+    
     columns = Math.floor(document.body.clientWidth / size);
     rows = Math.floor(document.body.clientHeight / size);
-
+    
     wrapper.style.setProperty("--columns", columns);
     wrapper.style.setProperty("--rows", rows);
-
+    
     createTiles(columns * rows);
-}
-
-createGrid();
-
-window.onresize = () => createGrid();
+  }
+  
+  createGrid();
+  
+  window.onresize = () => createGrid();
 
