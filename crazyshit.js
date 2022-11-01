c8cce12ecc09ff422520ec2284c4312a47ddb15d
pasekaalex <pasekaalex@gmail.com>
2022-11-01 17:20:10 -0400

    nice

--- crazyshit.js
+++ crazyshit.js
@@ -1,9 +1,36 @@
-let columns = Math.floor(document.body.clientWidth / 50),
-    rows = Math.floor(document.body.clientHeight / 50);
+const wrapper = document.getElementById("tiles");
+
+let columns = 0,
+    rows = 0;
+
+const colors = [
+    "rgb(229, 57, 53)",
+    "rgb(253, 216, 53)",
+    "rgb(244, 81, 30)",
+    "rgb(76, 175, 80)",
+    "rgb(33, 150, 243)",
+    "rgb(156, 39, 176)"
+];
+
+let count = -1;
+
+const handleOnClick = index => {
+    count = count + 1;
+
+    anime({
+        targets: ".tile",
+        backgroundColor: colors[count % (colors.length - 1)],
+        delay: anime.stagger(50, {
+            grid: [columns, rows], 
+            from: index
+        })
+    })
+}
 
 const createTile = index => {
     const tile = document.createElement("div");
     tile.classList.add("tile");
+    tile.onclick = e => handleOnClick(index);
     return tile;
 }
 const createTiles = quantity => {
